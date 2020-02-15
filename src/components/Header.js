687c6f1cbd5a41554b5a28a8aa15e64025818d3b
Julio Noguera <noguerajulio24@gmail.com>
2020-02-15 14:15:40 -0500

    adding styled component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 20px;
+    font-weight: bold;
+    color: #ffffff;
+`;
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
 const Header = ({titulo}) => {
     return(
-        <header>
-            <h2 className="text-center">{titulo}</h2>
-        </header>
+        <ContenedorHeader>
+            <TextHeader className="text-center">{titulo}</TextHeader>
+        </ContenedorHeader>
     )
 }
 
