d77eccf098a373284771847a3fbc3ffbad91270b
Julio Noguera <noguerajulio24@gmail.com>
2020-02-15 14:20:12 -0500

    adding styled component to the app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
+
 function App() {
   return (
-    <>
+    <Contenedor>
       <Header 
         titulo="Cotizador de Seguros"
       />
-    </>
+
+      <ContenedorForm>
+        
+      </ContenedorForm>
+    </Contenedor>
   );
 }
 
